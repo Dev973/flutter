6ce67de6c7ab6055092d86d07357c4bd0484c5a0
Dev973 <105357215+Dev973@users.noreply.github.com>
2023-05-13 15:52:11 +0530

    New Change

---
+++ main14.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  int n;
+  print("Enter the num: ");
+  n = int.parse(stdin.readLineSync()!);
+
+ int num = 0;
+ int num2 = 1;
+ 
+ print("Fibo series of $n");
+ for(int i =0; i<=n; i++){
+  print(num);
+  int total = num + num2;
+  num = num2;
+  num2 = total;
+ }
+}
